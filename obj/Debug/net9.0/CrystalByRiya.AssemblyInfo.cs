36481b68c93dc325d26df796331bc351458d6909
anildhakad8587 <anildhakad8587@gmail.com>
2026-04-04 19:09:25 +0530

    Fixed Total Quantity Issue

--- obj/Debug/net9.0/CrystalByRiya.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrystalByRiya.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrystalByRiya")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b48f7fe9f9986576e6df883a5ed456389312e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f399cfa71de3791a4267c62cb2745446fdbcd6c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrystalByRiya")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrystalByRiya")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
