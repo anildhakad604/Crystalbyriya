d64c1f044960af2a67476772c26f94d510d5c65b
anildhakad8587 <anildhakad8587@gmail.com>
2026-04-04 11:05:33 +0530

    fixed pagination issue

--- obj/Debug/net9.0/CrystalByRiya.AssemblyInfo.cs
+++ obj/Debug/net9.0/CrystalByRiya.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrystalByRiya")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558cb00fd6800bde51e174f58f402c8611d662df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe66b81bad7ba55923349c568ba970a17369ca6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrystalByRiya")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrystalByRiya")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
